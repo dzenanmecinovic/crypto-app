44138ecb2bc60c698cc97e1fb74c11e3c508746b
vranicmitar <mitarvranic0@gmail.com>
2023-06-13 00:21:25 +0200

    #update

--- src/pages/PricesPage/PricesPage.js
+++ src/pages/PricesPage/PricesPage.js
@@ -2,13 +2,7 @@ import React, { useState, useEffect } from "react";
 import Price from "../../components/Navbar/Price/Price";
 import "./PricesPage.css";
 import axios from "axios";
-// import Table from "@mui/material/Table";
-// import TableBody from "@mui/material/TableBody";
-// import TableCell from "@mui/material/TableCell";
-// import TableContainer from "@mui/material/TableContainer";
-// import TableHead from "@mui/material/TableHead";
-// import TableRow from "@mui/material/TableRow";
-// import Paper from "@mui/material/Paper";
+
 export default function PricesPage() {
   const [data, setData] = useState(null);
 
@@ -61,34 +55,6 @@ export default function PricesPage() {
             );
           })}
         </p>
-
-        // <TableContainer component={Paper}>
-        //   <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        //     <TableHead>
-        //       <TableRow>
-        //         <TableCell>Dessert (100g serving)</TableCell>
-        //         <TableCell align="right">Calories</TableCell>
-        //         <TableCell align="right">Fat&nbsp;(g)</TableCell>
-        //         <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-        //         <TableCell align="right">Protein&nbsp;(g)</TableCell>
-        //       </TableRow>
-        //     </TableHead>
-        //     <TableBody>
-        //       {data.map((coin) => (
-        //         <TableRow
-        //           key={coin.UUID}
-        //           sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-        //         >
-        //           <TableCell component="th" scope="row">
-        //             {coin.iconUrl}
-        //           </TableCell>
-        //           <TableCell align="right">{coin.name}</TableCell>
-        //           <TableCell align="right">{coin.price}</TableCell>
-        //         </TableRow>
-        //       ))}
-        //     </TableBody>
-        //   </Table>
-        // </TableContainer>
       )}
     </div>
   );
