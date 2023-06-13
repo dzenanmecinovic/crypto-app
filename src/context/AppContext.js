6aebe29c36051436943694681d19a960e95e4a9d
Dzenan Mecinovic <mecinovicd@gmail.com>
2023-06-13 16:39:52 +0200

    context

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,9 +1,18 @@
 import React, { createContext, useState } from "react";
-import data from "../pages/PricesPage/PricesPage";
 
 const AppContext = createContext();
 
 function ContextProvider({ children }) {
+
+    const [favorite, setFavorite]=useState(false);
+    
+    const toggleFavorite= (UUID) => {
+        if(data.filter(coin) => coin.UUID === UUID) {
+            setFavorite(true);
+        } else {
+            setFavorite(false);
+        }
+    }
   const values = {
     favorite,
     setFavorite,
