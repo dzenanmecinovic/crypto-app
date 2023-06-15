5a0f5334c33a6d739c81f2a1888813df8ecc5681
Dzenan Mecinovic <mecinovicd@gmail.com>
2023-06-15 19:45:29 +0200

    coins

--- src/pages/CoinsPage/CoinsPage.js
+++ src/pages/CoinsPage/CoinsPage.js
@@ -2,9 +2,5 @@ import React from "react";
 import "./CoinsPage.css";
 
 export default function CoinsPage() {
-  return (
-    <div className="coinsPage">
-      <h1>Test</h1>
-    </div>
-  );
+  return <div className="coinsPage"></div>;
 }
