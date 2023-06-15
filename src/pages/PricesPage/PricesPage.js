6324cad343267cee73f0470748ffbb0544507a68
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-06-15 14:57:47 +0200

    #newUpdate homepage

--- src/pages/PricesPage/PricesPage.js
+++ src/pages/PricesPage/PricesPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Price from "../../components/Navbar/Price/Price";
 import "./PricesPage.css";
 import axios from "axios";
