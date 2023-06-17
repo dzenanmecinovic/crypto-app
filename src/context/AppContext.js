6b530a519f10cb7054a2468293f217386cc2f57c
Dzenan Mecinovic <mecinovicd@gmail.com>
2023-06-17 16:03:50 +0200

    nista

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,13 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
 function ContextProvider({ children }) {
-  const [favorites] = useState([]);
-  const values = {
-    favorites,
+  const [data, setData] = useState({
+    coins: null,
+    favoriteCoins: [],
+  });
+
+  const addFavoriteCoin = (coinId) => {
+    setData((prevData) => {
+      const updatedFavoriteCoins = Array.isArray(prevData.favoriteCoins)
+        ? [...prevData.favoriteCoins, coinId]
+        : [coinId];
+
+      return {
+        ...prevData,
+        favoriteCoins: updatedFavoriteCoins,
+      };
+    });
   };
-  return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
+
+  return (
+    <AppContext.Provider value={{ data, addFavoriteCoin }}>
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export { AppContext, ContextProvider };
