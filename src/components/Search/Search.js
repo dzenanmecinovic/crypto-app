d743913b9a55ca30277b67c38c322501ccfcf406
vranicmitar <mitarvranic0@gmail.com>
2023-06-19 16:00:11 +0200

    searchUpdate

---
+++ src/components/Search/Search.js
@@ -0,0 +1,160 @@
+import {
+  Container,
+  InputAdornment,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { Sparklines, SparklinesLine } from "react-sparklines";
+
+export default function Search() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      url: "https://coinranking1.p.rapidapi.com/coins",
+      params: {
+        referenceCurrencyUuid: "yhjMzLPhuIDl",
+        timePeriod: "24h",
+        "tiers[0]": "1",
+        orderBy: "marketCap",
+        orderDirection: "desc",
+        limit: "20",
+        offset: "0",
+      },
+      headers: {
+        "X-RapidAPI-Key": "377b5ced87msh79d0645fafaa557p1c740fjsna2d6398a9888",
+        "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data.coins);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchData();
+    }, 500);
+  }, [data]);
+
+  // console.log(data);
+  // console.log(data.filter((coin) => coin.name));
+  return (
+    <>
+      {data === null && data === undefined ? (
+        <p>loading</p>
+      ) : (
+        <Container
+          maxWidth="md"
+          sx={{
+            position: "absolute",
+            top: 120,
+            left: "40%",
+            color: "white",
+          }}
+        >
+          <TextField
+            id="search"
+            type="search"
+            label="Search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            sx={{ width: 250, backgroundColor: "white", borderRadius: 1 }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Container>
+      )}
+      (
+      <TableContainer component={Paper} sx={{ width: "700px" }}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">#</TableCell>
+              <TableCell></TableCell>
+              <TableCell align="right" style={{ width: 100 }}>
+                Name
+              </TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Market Cap</TableCell>
+              <TableCell align="right">7 days</TableCell>
+              <TableCell align="right">Fav</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data
+              .filter((coin) => coin.name.toLowerCase().includes(query))
+              .map((coin) => (
+                <TableRow
+                  key={coin.UUID}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  onClick={() => navigate(`/coinscard/${coin.uuid}`)}
+                >
+                  <TableCell align="right">{coin.rank}</TableCell>
+                  <TableCell component="th" scope="row">
+                    <img src={coin.iconUrl} style={{ width: 25 }}></img>
+                  </TableCell>
+                  <TableCell align="right">{coin.name}</TableCell>
+                  <TableCell align="right">
+                    $${Number(coin.price).toLocaleString()}
+                  </TableCell>
+                  <TableCell align="right">
+                    ${Number(coin.marketCap).toLocaleString()}
+                  </TableCell>
+                  <TableCell align="right">
+                    <Sparklines
+                      data={coin.sparkline.map((el) => el)}
+                      height={100}
+                    >
+                      <SparklinesLine
+                        style={{
+                          stroke: "#559500",
+                          fill: "orange",
+                          fillOpacity: "1",
+                          stroke: "black",
+                          strokeWidth: 3,
+                        }}
+                      />
+                    </Sparklines>
+                  </TableCell>
+                  <TableCell align="right">
+                    <button
+                      style={{ border: "none", backgroundColor: "white" }}
+                    >
+                      <FavoriteBorderIcon
+                        style={{ color: "red" }}
+                      ></FavoriteBorderIcon>
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      ))
+    </>
+  );
+}
--- src/pages/PricesPage/PricesPage.js
+++ src/pages/PricesPage/PricesPage.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import Price from "../../components/Navbar/Price/Price";
 import "./PricesPage.css";
 import axios from "axios";
-
+import SearchIcon from "@mui/icons-material/Search";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import {
   Container,
@@ -19,13 +19,12 @@ import {
 // import { FavoriteBorder } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { Sparklines, SparklinesLine } from "react-sparklines";
-import { AppContext } from "../../context/AppContext";
-import SearchIcon from "@mui/icons-material/Search";
+import Search from "../../components/Search/Search";
 
 export default function PricesPage() {
   const [data, setData] = useState(null);
   const navigate = useNavigate();
-  const { searchTerm, handleChange } = useContext(AppContext);
+  const [query, setQuery] = useState("");
 
   const fetchData = async () => {
     const options = {
@@ -68,94 +67,7 @@ export default function PricesPage() {
           <p id="loading">Loading...</p>
         ) : (
           <>
-            <Container
-              maxWidth="md"
-              sx={{
-                position: "absolute",
-                top: 120,
-                left: "40%",
-                color: "white",
-              }}
-            >
-              <TextField
-                id="search"
-                type="search"
-                label="Search"
-                value={searchTerm}
-                onChange={handleChange}
-                sx={{ width: 250, backgroundColor: "white", borderRadius: 1 }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <SearchIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Container>
-            <TableContainer component={Paper} sx={{ width: "700px" }}>
-              <Table aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="right">#</TableCell>
-                    <TableCell></TableCell>
-                    <TableCell align="right" style={{ width: 100 }}>
-                      Name
-                    </TableCell>
-                    <TableCell align="right">Price</TableCell>
-                    <TableCell align="right">Market Cap</TableCell>
-                    <TableCell align="right">7 days</TableCell>
-                    <TableCell align="right">Fav</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map((coin) => (
-                    <TableRow
-                      key={coin.UUID}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                      onClick={() => navigate(`/coinscard/${coin.uuid}`)}
-                    >
-                      <TableCell align="right">{coin.rank}</TableCell>
-                      <TableCell component="th" scope="row">
-                        <img src={coin.iconUrl} style={{ width: 25 }}></img>
-                      </TableCell>
-                      <TableCell align="right">{coin.name}</TableCell>
-                      <TableCell align="right">
-                        $${Number(coin.price).toLocaleString()}
-                      </TableCell>
-                      <TableCell align="right">
-                        ${Number(coin.marketCap).toLocaleString()}
-                      </TableCell>
-                      <TableCell align="right">
-                        <Sparklines
-                          data={coin.sparkline.map((el) => el)}
-                          height={100}
-                        >
-                          <SparklinesLine
-                            style={{
-                              stroke: "#559500",
-                              fill: "orange",
-                              fillOpacity: "1",
-                              stroke: "black",
-                              strokeWidth: 3,
-                            }}
-                          />
-                        </Sparklines>
-                      </TableCell>
-                      <TableCell align="right">
-                        <button
-                          style={{ border: "none", backgroundColor: "white" }}
-                        >
-                          <FavoriteBorderIcon
-                            style={{ color: "red" }}
-                          ></FavoriteBorderIcon>
-                        </button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+            <Search />
           </>
         )}
       </div>
