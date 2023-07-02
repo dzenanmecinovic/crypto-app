65ac9ac5528a339c88a16622512df55d3b2ab216
Dzenan Mecinovic <mecinovicd@gmail.com>
2023-07-03 00:49:59 +0200

    gg

--- src/pages/PricesPage/PricesPage.js
+++ src/pages/PricesPage/PricesPage.js
@@ -40,6 +40,7 @@ export default function PricesPage() {
       fetchData();
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, 2000);
+    // eslint-disable-next-line
   }, []);
 
   return (
